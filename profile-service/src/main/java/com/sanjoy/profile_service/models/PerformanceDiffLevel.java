ed2c267ab01c7213540005ef3429328217782dbc
kumar <kumarsanjoy.shuvo@gmail.com>
2025-08-05 12:06:45 +0600

    subject progress in profile-service added

---
+++ profile-service/src/main/java/com/sanjoy/profile_service/models/PerformanceDiffLevel.java
@@ -0,0 +1,26 @@
+package com.sanjoy.exam_service.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/**
+ * @author kumar
+ * @since 7/24/2025
+ */
+
+@Data
+@Entity
+public class PerformanceDiffLevel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String subject;
+    private int performance;
+    private int difficultyLevel;
+    private boolean isCorrect;
+}
