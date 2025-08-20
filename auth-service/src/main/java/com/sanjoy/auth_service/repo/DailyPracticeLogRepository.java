77943ee14fdfad2d8473aaa43dd4011f337f72bc
kumar <kumarsanjoy.shuvo@gmail.com>
2025-08-20 16:59:32 +0600

    everything except python, frontend is now docker ready

---
+++ auth-service/src/main/java/com/sanjoy/auth_service/repo/DailyPracticeLogRepository.java
@@ -0,0 +1,17 @@
+package com.sanjoy.exam_service.repo;
+
+import com.sanjoy.exam_service.models.DailyPracticeLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * @author kumar
+ * @since 8/3/2025
+ */
+public interface DailyPracticeLogRepository extends JpaRepository<DailyPracticeLog, Long> {
+    List<DailyPracticeLog> findByUsernameOrderByPracticedOnDesc(String username);
+    boolean existsByUsernameAndPracticedOn(String username, LocalDate practicedOn);
+    void deleteByUsername(String username);
+}
